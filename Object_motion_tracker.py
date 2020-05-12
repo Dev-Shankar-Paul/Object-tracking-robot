c138738c7494ba37dbdacac488325c8771ba4a63
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-05-12 10:56:04 +0530

    Stabilised readings

--- Object_motion_tracker.py
+++ Object_motion_tracker.py
@@ -7,9 +7,12 @@ Created on Sat May  9 21:15:32 2020
 """
 import cv2
 import numpy as np
+from statistics import mode
 
 cap = cv2.VideoCapture(0)
 
+# Create the trackbars to manually set the hue, saturation 
+# and value of the image to be tracked 
 cv2.namedWindow("Trackbars")
 
 cv2.createTrackbar("L-H", "Trackbars", 0, 179, lambda x:x)
@@ -18,15 +21,26 @@ cv2.createTrackbar("L-V", "Trackbars", 0, 255, lambda x:x)
 cv2.createTrackbar("U-H", "Trackbars", 0, 179, lambda x:x)
 cv2.createTrackbar("U-S", "Trackbars", 0, 255, lambda x:x)
 cv2.createTrackbar("U-V", "Trackbars", 0, 255, lambda x:x)
-    
-lower_blue = np.array([110,50,50])
-upper_blue = np.array([130,255,255])
+
+x_max = 640
+y_max = 480
+
+# Setting the values of the corner-points (clockwise from top-left)
+c1 = (0, 0)
+c2 = (x_max, 0)
+c3 = (x_max, y_max)
+c4 = (0, y_max)
 
 while True:
     
     ret, frame = cap.read()
+    #frame = cv2.GaussianBlur(frame, (5,5), 3)
+    
+    # convert the image to HSV to allow InRange to work on it
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
+    # prep the taskbars and get the values from them by manual
+    # adjustment
     lh = cv2.getTrackbarPos("L-H", "Trackbars")
     ls = cv2.getTrackbarPos("L-S", "Trackbars")
     lv = cv2.getTrackbarPos("L-V", "Trackbars")
@@ -34,14 +48,49 @@ while True:
     us = cv2.getTrackbarPos("U-S", "Trackbars")
     uv = cv2.getTrackbarPos("U-V", "Trackbars")
     
+    # Thresholding the image to obtain the mask
     mask = cv2.inRange(hsv, np.array([lh, ls, lv]), 
                        np.array([uh, us, uv]))
+    #cv2.imshow('Mask', mask)
     
-    final = cv2.bitwise_and(frame, frame, mask = mask)
-    cv2.imshow('Original', frame)
-    cv2.imshow('Mask', mask)
-    cv2.imshow('Final', final)
+    # performing bitwise AND operation to layer mask over frame
+    #final = cv2.bitwise_and(frame, frame, mask = mask)
+    #cv2.imshow('Final', final)
     
+    # utilise the mask to get contours and plot them on frame
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_NONE)
+    #cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+    #cv2.imshow('Contours', frame)
+     
+    # If contours is not an empty list, loop through it to find
+    # the center of the contour using moments 
+    list_cx = []
+    list_cy = []
+    if(contours != []):
+        for c in contours:
+            M = cv2.moments(c)
+            if(M["m00"] != 0):
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+            else:
+                cx, cy = cx, cy
+    else:
+        cx, cy = int(x_max/2), int(y_max/2)
+    
+    # drawing the line which is to be used to find angle
+    cv2.line(frame, (int(x_max/2), 0), (int(x_max/2), int(y_max)),
+             (0, 255, 0), 3)
+    
+    # Draw a circle showing the part that was detected. The list thing
+    # is used to stabilise the readings a little bit. Ask Tony Bhaiya.
+    list_cx.append(cx)
+    list_cy.append(cy)
+    cv2.circle(frame, (mode(list_cx),mode(list_cy)),
+               1, (0, 255, 0), 3)
+    
+    frame = cv2.flip(frame, 1)
+    cv2.imshow('Object detected', frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
     
